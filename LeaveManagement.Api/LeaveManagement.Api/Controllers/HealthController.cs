7e91e81cd534f0954fc4d8392eb387f1f4856150
NadyaSudusinghe <ndsudusinghe@gmail.com>
2026-01-14 21:19:42 +0100

    Create ASP.NET Web API project with health endpoint (.NET 9)

---
+++ LeaveManagement.Api/LeaveManagement.Api/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagement.Api.Controllers
+{
+    [Route("api/health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "Healthy",
+                serverTimeUtc = DateTime.UtcNow
+            });
+        }
+    }
+}
