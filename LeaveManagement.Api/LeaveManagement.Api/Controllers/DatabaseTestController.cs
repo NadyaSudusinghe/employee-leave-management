1352a31003f325dcb26c4e46baf037e6b0c824dd
NadyaSudusinghe <ndsudusinghe@gmail.com>
2026-01-15 20:58:46 +0100

    Connect ASP.NET API to PostgreSQL using EF Core

---
+++ LeaveManagement.Api/LeaveManagement.Api/Controllers/DatabaseTestController.cs
@@ -0,0 +1,25 @@
+﻿using LeaveManagement.Api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagement.Api.Controllers
+{
+    [Route("api/db-test")]
+    [ApiController]
+    public class DatabaseTestController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DatabaseTestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TestConnection()
+        {
+            var canConnect = await _context.Database.CanConnectAsync();
+            return Ok(new { coonected = canConnect });
+        }
+    }
+}
--- LeaveManagement.Api/LeaveManagement.Api/Program.cs
+++ LeaveManagement.Api/LeaveManagement.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using LeaveManagement.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace LeaveManagement.Api
 {
     public class Program
@@ -8,6 +11,12 @@ namespace LeaveManagement.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseNpgsql(
+                    builder.Configuration.GetConnectionString("DefaultConnection")
+                )
+            );
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
