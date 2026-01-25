ccd0549b5ca082809399aa2b5a3f161be4f314e6
NadyaSudusinghe <ndsudusinghe@gmail.com>
2026-01-25 20:08:54 +0100

    Add CRUD endpoints for Employees table

---
+++ LeaveManagement.Api/LeaveManagement.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,87 @@
+﻿using LeaveManagement.Api.Data;
+using LeaveManagement.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Api.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Create new Employee
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+        }
+
+        //Get all records
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        //Get By Id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        //Update Employe
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id, Employee updatedEmployee)
+        {
+            if(id != updatedEmployee.Id)
+                return BadRequest("ID mismatch");
+
+            var existingEmployee = await _context.Employees.FindAsync(id);
+
+            if (existingEmployee == null)
+                return NotFound();
+
+            existingEmployee.FirstName = updatedEmployee.FirstName;
+            existingEmployee.LastName = updatedEmployee.LastName;
+            existingEmployee.Email = updatedEmployee.Email;
+            existingEmployee.DateJoined = updatedEmployee.DateJoined;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //Delete Employee
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if(employee == null)
+                return NotFound();
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
